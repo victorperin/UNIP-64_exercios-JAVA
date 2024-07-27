8f4349dd862d336c2fd45d12ec94f73954016804
Antonioafj <antonioafj.edu@gmail.com>
2024-07-27 09:13:49 -0400

    calcula comissao do vendedor padrao CamelCase adicionado

--- Lista - 64 Exercicios/3/Comissao.java
+++ Lista - 64 Exercicios/3/Comissao.java
@@ -8,31 +8,36 @@
  * 		- Quantidade vendida
 
 *************************************************************************/
+
 import java.util.Scanner;
 
+/**
+ * Comissao
+ */
 public class Comissao {
+
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-		System.out.print("Programa para pagamento de comissão de vendedores de peças.\n\n"); //Why so specific?!
+		System.out.println("Programa par pagamento de comissão de vendedores de peças. \n\n"); //Why so specific?!
 		
-		int IdVendedor,IdPeca;
-		float PrecoPeca,Quantidade,ValorComissao,PorcentagemComissao=0.05f;
-		
-		System.out.print("Digite a identificação do vendedor: ");
-		IdVendedor = entrada.nextInt();
-		
-		System.out.print("Digite o ID da Peça: ");
-		IdPeca = entrada.nextInt();
-		
-		System.out.print("Digite o preço unitário da peça: ");
-		PrecoPeca = entrada.nextFloat();
-		
-		System.out.print("Digite a quantidade de peças vendidas: ");
-		Quantidade = entrada.nextFloat();
-		
-		ValorComissao = (PrecoPeca * Quantidade) * PorcentagemComissao; 
-		
-		System.out.printf("O valor da comissão é de %.2f reais.\n", ValorComissao);
-		
+		int idVendedor, idPeca;
+		float precoPeca, quantidade, valorComissao, porcentagemComissao = 0.05f;
+
+		System.out.println("Digite a identificação do vendedor: ");
+		idVendedor = entrada.nextInt();
+
+		System.out.println("Digite o ID da peça: ");
+		idPeca = entrada.nextInt();
+
+		System.out.println("Digite o preço unitario da peca: ");
+		precoPeca = entrada.nextFloat();
+
+		System.out.println("Digite a quantidade de peças vendidas");
+		quantidade = entrada.nextFloat();
+
+		valorComissao = (precoPeca * quantidade) * porcentagemComissao;
+
+		System.out.printf("O valor da comissao é de %.2f reis.\n", valorComissao);
+
 	}
 }
