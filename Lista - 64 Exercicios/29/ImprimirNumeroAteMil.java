de204cfdfc72b5f28d89860cae37791960c36a99
Antonioafj <antonioafj.edu@gmail.com>
2024-08-04 11:07:01 -0400

    exercicio 29 classe renomeada reescrito codigo

--- Lista - 64 Exercicios/29/ImprimirNumeroAteMil.java
+++ Lista - 64 Exercicios/29/ImprimirNumeroAteMil.java
@@ -1,14 +1,15 @@
 /*******************************************************************************
 * 29) Faça um programa que exiba os números de 1 até 2000.
 *******************************************************************************/
-public class Exercicio {
+/**
+ * ImprimirNumeroAteMil
+ */
+public class ImprimirNumeroAteMil {
 
-  public static void main(String[] args) {
-
-    for(int x=1; x <= 2000;x++){
+    public static void main(String[] args) {
+      
+      for(int x = 1; x <= 2000; x++){
         System.out.println(x);
+      }
     }
-
-
-  }
 }
