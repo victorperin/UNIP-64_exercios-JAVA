f9b9dec3770ff29dc05829dc6899bc3deeafd277
Antonioafj <antonioafj.edu@gmail.com>
2024-08-10 13:18:34 -0400

    exercicio 45 corrigido codigo refeito renomeada classe

--- Lista - 64 Exercicios/45/ImprimirOrdemInversa.java
+++ Lista - 64 Exercicios/45/ImprimirOrdemInversa.java
@@ -2,27 +2,32 @@
 * 45) Dada uma seqüência de n números (vetor de inteiros), imprimi-la na ordem inversa que
 * foi realizada a leitura.
 *******************************************************************************/
+
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Exercicio {
+/**
+ * ImprimirOrdemInversa
+ */
+public class ImprimirOrdemInversa {
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 		int quantidade;
-		
-		System.out.print("Digite a quantidade de números: ");
-		quantidade = entrada.nextInt();
-		
-		int[] Numeros = new int[quantidade];
-		
-		for(int x=0;x<quantidade;x++){
-			System.out.print("Digite o "+(x+1)+"° Número: ");
-			Numeros[x]= entrada.nextInt();
-		}
-		System.out.print("\nA ordem contrária é:\n");
-		for(int y=quantidade-1;y>=0;y--){
-			System.out.println(Numeros[y]);
-		}
 
+		System.out.println("Digite a quantidade de números: ");
+		quantidade = entrada.nextInt();
+
+		int[] numeros = new int[quantidade];
+
+		for(int x=0; x< quantidade; x++) {
+			System.out.println("Digite o " + (x+1) + " ° Número: ");
+			numeros[x] = entrada.nextInt();
+		}
+		
+		for (int j =quantidade-1; j >= 0; j--) {
+            System.out.println(numeros[j]);
+        }
+		
 	}
 }
