31fd15756774c263593912d786ac7a36c9a095c7
Antonioafj <antonioafj.edu@gmail.com>
2024-08-16 08:52:57 -0400

    exercio  1  da lista dois reescrito codigo -  treino

--- Lista 2/1/CanetaTeste.java
+++ Lista 2/1/CanetaTeste.java
@@ -1,26 +1,23 @@
 /*******************************************************************************
-* 1) Implementar uma classe Caneta que deve possuir como características marca,
-*	cor e tamanho. Nesta classe devem ser implementados os métodos construtores,
-*	getters, setters e toString. Em uma outra classe chamada CanetaTeste deverá
+* 1) Implementar uma classe Caneta que deve possuir como caracterï¿½sticas marca,
+*	cor e tamanho. Nesta classe devem ser implementados os mï¿½todos construtores,
+*	getters, setters e toString. Em uma outra classe chamada CanetaTeste deverï¿½
 *	ser criado um objeto do tipo Caneta, atribuir valores e exibir os dados
 *	deste objeto.
 *******************************************************************************/
-
-
-import java.util.Scanner;
-import Caneta;
-
+/**
+ * CanetaTeste
+ */
 public class CanetaTeste {
 
     public static void main(String[] args) {
         Caneta bic = new Caneta();
         bic.setMarca("bic");
-        bic.setCor("azul");
+        bic.setCor("Azul");
         bic.setTamanho("10cm");
-        
-        System.out.println("Tamanho: "+bic.getTamanho());
-        System.out.println("Cor: "+bic.getCor());
-        System.out.println("Marca: "+bic.getMarca());
+
+        System.out.println("Tamanho: " + bic.getTamanho());
+        System.out.println("Cor: "+ bic.getCor());
+        System.out.println("Marca: "+ bic.getMarca());
     }
-    
 }
