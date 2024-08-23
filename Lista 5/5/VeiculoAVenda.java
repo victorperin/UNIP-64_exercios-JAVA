f2b89bbc60ce88242cecfc452b9977a55c95da35
Antonioafj <antonioafj.edu@gmail.com>
2024-08-23 09:04:48 -0400

    Lista 5 exercio 5 refeito - treino

--- Lista 5/5/VeiculoAVenda.java
+++ Lista 5/5/VeiculoAVenda.java
@@ -5,7 +5,11 @@ Escreva uma classe VeiculoAVenda que represente informações básicas sobre um 
 • Escreva uma classe MotocicletaAVenda que herde de VeiculoAVenda e que inicialize o campo tipo com o valor "Motocicleta".
 • Escreva também um programa que demonstre o uso destas classes usando Polimorfismo.
 *******************************************************************************/
-public class VeiculoAVenda{
+/**
+ * VeiculoAVenda
+ */
+public class VeiculoAVenda {
+
   private String tipo;
   private int ano;
   private float precoDeVenda;
@@ -15,35 +19,40 @@ public class VeiculoAVenda{
     this.ano = ano;
     this.precoDeVenda = precoDeVenda;
   }
+
   protected VeiculoAVenda(){}
 
   //getters
   public String getTipo(){
     return this.tipo;
   }
+
   public int getAno(){
     return this.ano;
   }
+
   public float getPrecoDeVenda(){
     return this.precoDeVenda;
   }
+
 
   //setters
   public void setTipo(String tipo){
     this.tipo = tipo;
   }
+
   public void setAno(int ano){
     this.ano = ano;
   }
-  public void setPercodeVenda(float precoDeVenda){
+  public void setPrecoVenda(float precoDeVenda){
     this.precoDeVenda = precoDeVenda;
   }
 
-  //toString
   public String toString(){
-    String retorno = "Tipo: "+this.tipo;
-    retorno += "\nAno: "+this.ano;
-    retorno += "\nPreço: "+this.precoDeVenda+"\n";
+    String retorno = "Tipo: " + this.tipo;
+    retorno += "\nAno: "+ this.ano;
+    retorno += "\nPreço"+this.precoDeVenda+"\n";
     return retorno;
   }
+
 }
